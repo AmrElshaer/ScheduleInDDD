e22334299ea59edb971937be2a2e49782f74ce4f
Amr Elshaer <amrelsher07@gmail.com>
2023-02-25 15:24:22 +0200

    add schedule

---
+++ ScheduleInDDD.Core/Events/AppointmentConfirmedEvent.cs
@@ -0,0 +1,22 @@
+﻿using PluralsightDdd.SharedKernel;
+using ScheduleInDDD.Core.ScheduleAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleInDDD.Core.Events
+{
+    public class AppointmentConfirmedEvent : BaseDomainEvent
+    {
+        public AppointmentConfirmedEvent(Appointment appointment)
+        {
+            AppointmentUpdated = appointment;
+        }
+
+        public Guid Id { get; private set; } = Guid.NewGuid();
+
+        public Appointment AppointmentUpdated { get; private set; }
+    }
+}
