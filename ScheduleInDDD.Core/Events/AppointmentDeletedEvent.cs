6ca2853a7d1e661fab3bf11a446703d1245e2db7
Amr Elshaer <amrelsher07@gmail.com>
2023-02-27 07:39:48 +0200

    add delete event

---
+++ ScheduleInDDD.Core/Events/AppointmentDeletedEvent.cs
@@ -0,0 +1,21 @@
+﻿using PluralsightDdd.SharedKernel;
+using ScheduleInDDD.Core.ScheduleAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleInDDD.Core.Events
+{
+    internal class AppointmentDeletedEvent : BaseDomainEvent
+    {
+        public AppointmentDeletedEvent(Appointment appointment)
+        {
+            AppointmentDeleted = appointment;
+        }
+
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public Appointment AppointmentDeleted { get; private set; }
+    }
+}
---
+++ ScheduleInDDD.Core/Interfaces/IScheduleRepository.cs
@@ -0,0 +1,15 @@
+﻿using ScheduleInDDD.Core.ScheduleAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleInDDD.Core.Interfaces
+{
+    public interface IScheduleRepository
+    {
+        Schedule GetScheduleForDate(int clinicId, DateTime date);
+        void Update(Schedule schedule);
+    }
+}
